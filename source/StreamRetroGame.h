431299c082bee1c99e024edb099f4b704805db86
Orell Garten <orellgarten@gmx.de>
2020-03-29 17:12:17 +0200

    minor change to clang format

---
+++ source/StreamRetroGame.h
@@ -0,0 +1,16 @@
+//
+// Created by orell on 3/29/20.
+//
+
+#ifndef STREAMRETRO_STREAMRETROGAME_H
+#define STREAMRETRO_STREAMRETROGAME_H
+
+
+
+class StreamRetroGame {
+
+};
+
+
+
+#endif //STREAMRETRO_STREAMRETROGAME_H
